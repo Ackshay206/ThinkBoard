46177a6483e20c190145b3d809da45e062fcd6a9
Ackshay Nagamallu Rajasekar <ackshay.rajasekar26@gmail.com>
2025-10-19 18:18:46 -0400

    Controller modified

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,16 +1,32 @@
-export function getAllNotes(req,res)  {
-    res.status(200).send("you fetched the notes");
+import Note from '../models/noteModel.js';
+
+export async function getAllNotes(req,res) {
+    try {
+        const notes = await Note.find();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error in getALLNotes controller", error);
+        res.status(500).json({message: "Error retrieving notes"});
+    }
     
 }
 
-export function createNote(req,res) {
-    res.status(201).json({message: "your note has been created successfully"});
+export async function createNote(req,res) {
+    try {
+        const { title, content } = req.body;
+        const newNote = new Note({ title, content });
+        await newNote.save();
+        res.status(201).json(newNote);
+    } catch (error) {
+        console.error("Error in createNote controller", error);
+        res.status(500).json({message: "Error creating note"});
+    }
 }
 
-export function updateNote(req,res) {
+export async function updateNote(req,res) {
     res.status(200).json({message: "your note has been updated successfully"});
 }
 
-export function deleteNote(req,res) {
+export async function deleteNote(req,res) {
     res.status(200).json({message: "your note has been deleted successfully"});
 }
