ea653040d9c70197c16e8074942cc99d33a9bd81
Ackshay Nagamallu Rajasekar <ackshay.rajasekar26@gmail.com>
2025-10-25 22:46:51 -0400

    tailwind css implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage.jsx';
-import CreatePage from './pages/CreatePage.jsx';
-import NoteDetailPage from './pages/NoteDetailPage.jsx';
-import { toast } from 'react-hot-toast';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
+import NoteDetailPage from "./pages/NoteDetailPage.jsx";
+import { toast } from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
-      <button onClick={() => toast.success("congrats")}>Click me</button>
+    <div data-theme="forest">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
